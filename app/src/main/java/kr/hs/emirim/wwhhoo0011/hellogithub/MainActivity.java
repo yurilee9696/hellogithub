b3ddce77e8d6c3c320a214764ed619c8b162161c
YuRi Lee <yurilee9696@gmail.com>
2017-03-28 20:54:49 +0900

    #2 버튼 누르면 토스트 메시지

--- app/src/main/java/kr/hs/emirim/wwhhoo0011/hellogithub/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/wwhhoo0011/hellogithub/MainActivity.java
@@ -2,12 +2,23 @@ package kr.hs.emirim.wwhhoo0011.hellogithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnHello = (Button)findViewById(R.id.btnHello); //R = 리소스
+        btnHello.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this,"SHINING DIAMONDS",Toast.LENGTH_SHORT).show();
     }
 }
